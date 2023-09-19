7989dee0261d59f60c8991a43cdf6cebb6b4b580
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-19 05:40:51 -0400

    Add do now and code exercises

---
+++ events.js
@@ -0,0 +1,63 @@
+// stored html elements
+let mario = document.querySelector(".mario");
+let coin = document.querySelector(".coin");
+let score = document.querySelector(".score");
+let title = document.querySelector("h1");
+let background = document.querySelector("body");
+let jumpButton = document.querySelector(".jump");
+let jumpDownButton = document.querySelector(".jump-down");
+let changeColorButton = document.querySelector(".change-color");
+let endGameButton = document.querySelector(".end-game");
+let updateScoreButton = document.querySelector(".update-score");
+let growButton = document.querySelector(".grow");
+
+/* 1. When jump button is clicked, Mario jumps and coin disappears.  
+
+________.addEventListener("click", function() {
+    mario.style.bottom = "120px";
+    coin.style.display = "none";
+});
+
+*/
+
+
+/* 2. When jump down button is clicked, Mario jumps down. 
+
+________.addEventListener("click", function() {
+    mario.style = "bottom: 35px";
+});
+
+*/
+
+
+/* 3. When change color button is clicked, background color changes. 
+
+________.addEventListener("click", function() {
+    background.style.backgroundColor = "skyblue";
+});
+
+*/
+
+
+/* 4. When end game button is clicked, the title of the page says game over. 
+
+________._______ ("___", ________()
+    title.innerHTML = "Game over.";
+});
+
+*/
+
+
+/* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
+
+________._______ ("___", ________()
+    score.innerHTML = 1;
+});
+
+*/
+
+
+/*6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
+
+
+*/
---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.querySelector("button");
+                                    
+// 1. Declare a variable called `answer` that stores the p tag that has the class name `answer`.
+
+
+
+button.addEventListener("click", function() {
+// 2. Replace the value with a string with a name of a movie you enjoy.
+// 3. Uncomment the line of code below, run, and try the button.
+// answer.innerHTML="movie name goes here";
+
+
+
+
+
+});
